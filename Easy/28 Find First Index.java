91e3f89034b5af476357cd7dbc9ade85910a83a3
Will <43940033+WillDaru22@users.noreply.github.com>
2025-01-13 02:22:35 -0600

    Create 28 Find First Index.java

---
+++ Easy/28 Find First Index.java
@@ -0,0 +1,7 @@
+// For https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return(haystack.indexOf(needle));  // Easy solution in Java with the indexOf() method
+    }
+}
