479a577cf41b301875acccf87ffb065ff5661383
Will <43940033+WillDaru22@users.noreply.github.com>
2025-02-17 19:05:01 -0600

    Create 62 Unique Paths.java

---
+++ Medium/62 Unique Paths.java
@@ -0,0 +1,18 @@
+// For the problem at https://leetcode.com/problems/unique-paths/
+
+class Solution {
+    public int uniquePaths(int m, int n) {  // attempting to solve this using dynamic programming
+        int[][] dp = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == 0 || j == 0) {
+                    dp[i][j] = 1;
+                }
+                else {
+                    dp[i][j] = dp[i][j-1] + dp[i-1][j];
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
